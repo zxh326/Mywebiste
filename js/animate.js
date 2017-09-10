df67932cb96cbc04ee201d076d7020e986eb501d
zxh326 <zhangxh1997@gmail.com>
2017-09-10 13:42:44 +0800

    js

---
+++ js/animate.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function() {
+    var delay = 1;
+    var DELAY_STEP = 200;
+    var animationOptions = {
+        "opacity": 1,
+        "top": 0
+    };
+
+    function animateMain() {
+        var dfd = $.Deferred();
+        var els = $(".animate-init");
+        var length = els.length;
+
+        els.each(function(index, el) {
+            delay++;
+            $(el).delay(index * DELAY_STEP).animate(animationOptions);
+            (length - 1 === index) && dfd.resolve();
+        });
+
+        return dfd.promise();
+    }
+
+    animateMain();
+});
